0a25c1b6f3ed03e756b13d30115bbcceb45b728c
Anmol Diwedi <ianmol3107@gmail.com>
2023-11-19 04:25:37 +0530

    Add files via upload

---
+++ Leetcode_162.java
@@ -0,0 +1,20 @@
+public class Leetcode_162 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1};
+        System.out.println(findPeakElement(arr));
+    }
+    static int findPeakElement(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                end = mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
